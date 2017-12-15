0c7a4f3b179a5f518a8ef12f0459e8476c95da35
Richard Allison <richard.allison.18@outlook.com>
2017-12-15 20:35:20 +0000

    Restructured so that program is run from app.rb in root directory and connects to stock_controller in controllers directory, and added new stock directory inside views directory for actions connected to stock

---
+++ controllers/stock_controller.rb
@@ -0,0 +1,11 @@
+require("sinatra")
+require("sinatra/contrib/all") if development?
+require_relative("../models/genre")
+require_relative("../models/artist")
+require_relative("../models/album")
+require_relative("../models/stock")
+
+get("/stock") do
+  @stocks = Stock.all()
+  erb(:"stock/index")
+end
